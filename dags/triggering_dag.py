f64db85a5f53f349b843fc9b6518703edb3511c9
ahotwani <66929452+ahotwani@users.noreply.github.com>
2024-10-25 09:23:06 -0500

    Create triggering_dag.py

---
+++ dags/triggering_dag.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from airflow.models.dag import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+with DAG(dag_id="triggering_dag", schedule=None, start_date=datetime(2023, 1, 1)):
+    SimpleHttpOperator(
+        task_id="trigger_external_dag",
+        log_response=True,
+        method="POST",
+            # Change this to the DAG_ID of the DAG you are triggering
+        endpoint=f"api/v1/dags/hello_world/dagRuns",
+        http_conn_id="http_conn",
+        data={
+            "logical_date": "{{ logical_date }}",
+
+            # if you want to add parameters:
+            # params: '{"foo": "bar"}'
+        }
+    )
